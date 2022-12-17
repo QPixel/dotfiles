7196d80770739fcf99d3b3078ded817886ceb32b
Riley Smith <riley@rileysmith.me>
2022-12-16 22:05:00 -0800

    chore(sway): update sway launcher script

--- executable_launch-sway.sh
+++ executable_launch-sway.sh
@@ -2,6 +2,7 @@
 export SDL_VIDEODRIVER=wayland
 export _JAVA_AWT_WM_NONREPARENTING=1
 export QT_QPA_PLATFORM=wayland
+#export QT_STYLE_OVERRIDE=adwaita
 export QT_QPA_PLATFORMTHEME=qt5ct
 export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
 export BEMENU_BACKEND=wayland
@@ -11,6 +12,9 @@ export WLR_DRM_NO_MODIFIERS=1
 export XDG_CURRENT_DESKTOP=sway
 export XDG_SESSION_DESKTOP=sway
 export MOZ_DBUS_REMOTE=1
+#export GTK_DEBUG=portals
+#export GTK_USE_PORTAL=1
 export MOZ_ENABLE_WAYLAND=1
 #exec $HOME/pipewire.sh
-exec dbus-run-session sway
+export GTK_THEME="$(grep 'gtk-theme-name' "${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini" | sed 's/.*\s*=\s*//')"
+exec sway
